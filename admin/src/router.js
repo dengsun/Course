b4ec31fecc046423d48f8450b1bfe9d8cea31274
dengsun <1515169863@qq.com>
2023-05-02 14:38:10 +0800

    1.增加admin页面 2.增加路由配置，访问/admin时，加载admin组件

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,18 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/login.vue'
-import login from "@/views/login.vue";
+import Admin from './views/admin.vue'
 
 Vue.use(Router);
 
 export default new Router({
-    mode: "history",
+    mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
         path: "*",
         redirect: "/login",
     }, {
         path: '/login',
-        component: login
+        component: Login
+    }, {
+        path: '/admin',
+        component: Admin
     }]
 })
