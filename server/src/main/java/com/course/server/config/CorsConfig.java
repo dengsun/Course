3f04c5d7d5baf7428d97c5a017e6472247fd6b0f
dengsun <1515169863@qq.com>
2023-05-03 14:24:09 +0800

    1.大章页面显示真实数据 2.修改CorsConfig，彻底解决前后端跨域的问题，原视频的代码不行

--- server/src/main/java/com/course/server/config/CorsConfig.java
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -12,9 +12,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
                 .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
     }
 
