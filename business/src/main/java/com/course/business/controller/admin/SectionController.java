ed012033ad78d81d75672ac041bfc334a2b5d5ac
dengsun <1515169863@qq.com>
2023-05-06 07:29:24 +0800

    1.生成器开发：前后端模板增加字段校验

--- business/src/main/java/com/course/business/controller/admin/SectionController.java
+++ business/src/main/java/com/course/business/controller/admin/SectionController.java
@@ -1,6 +1,5 @@
 package com.course.business.controller.admin;
 
-import com.course.server.domain.Section;
 import com.course.server.dto.SectionDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
@@ -11,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/section")
@@ -39,6 +37,9 @@ public class SectionController {
     @PostMapping("/save")
     public ResponseDto save(@RequestBody SectionDto sectionDto){
         // 保存校验
+        ValidatorUtil.require(sectionDto.getTitle(), "标题");
+        ValidatorUtil.length(sectionDto.getTitle(), "标题", 1, 50);
+        ValidatorUtil.length(sectionDto.getVideo(), "视频", 1, 200);
 
         ResponseDto responseDto = new ResponseDto();
         sectionService.save(sectionDto);
