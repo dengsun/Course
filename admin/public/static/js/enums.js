5b2524dbc269471c8534524f601572fcc60bc38a
dengsun <1515169863@qq.com>
2023-05-06 12:02:14 +0800

    1.增加枚举生成器EnumGenerator 2.optionKV过滤器从list改为object

--- admin/public/static/js/enums.js
+++ admin/public/static/js/enums.js
@@ -1,4 +1,5 @@
 CHARGE=[{key:"C", value:"收费"},{key:"F", value:"免费"}];
+SECTION_CHARGE={CHARGE:{key:"C", value:"收费123"},FREE:{key:"F", value:"免费321"}};
 
 // SECTION_CHARGE={CHARGE:{key:"C", value:"收费"},FREE:{key:"F", value:"免费"}};
 // YES_NO={YES:{key:"1", value:"是"},NO:{key:"0", value:"否"}};
