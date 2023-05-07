615f82112ff11cdbfd4eb3be45e8c66e77c45ffc
dengsun <1515169863@qq.com>
2023-05-07 09:39:59 +0800

    1.大章管理保存和查询功能管理courseId

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -64,10 +64,10 @@
                   <input v-model="chapter.name" class="form-control" placeholder="名称">
                 </div>
               </div>
-              <div class="control-group">
-                <label class="col-sm-2 control-label">课程ID</label>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">课程</label>
                 <div class="col-sm-10">
-                  <input v-model="chapter.courseId" class="form-control" placeholder="课程ID">
+                  <p class="form-control-static">{{course.name}}</p>
                 </div>
               </div>
             </form>
@@ -129,6 +129,7 @@ export default {
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/list',{
         page: page,
         size: _this.$refs.pagination.size,
+        courseId: _this.course.id
       }).then((response)=>{
         Loading.hide();
         let resp = response.data;
@@ -146,13 +147,13 @@ export default {
 
       // 保存校验
       if (!Validator.require(_this.chapter.name, "名称")
-        || !Validator.require(_this.chapter.courseId, "课程ID")
         || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
         return;
       }
+      _this.chapter.courseId = _this.course.id;
 
       Loading.show();
-      _this.$ajax.post(process.env.VUE_APP_SERVER + 'business/admin/chapter/save',_this.chapter).then((response)=>{
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/save',_this.chapter).then((response)=>{
         Loading.hide();
         let resp = response.data;
         if(resp.success){
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,7 +3,7 @@ package com.course.server.service;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
-import com.course.server.dto.PageDto;
+import com.course.server.dto.ChapterPageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.course.server.util.CopyUtil;
 import com.course.server.util.UuidUtil;
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,14 +24,18 @@ public class ChapterService {
     /**
      * 列表查询
      */
-    public void list(PageDto pageDto){
-        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
+    public void list(ChapterPageDto chapterPageDto){
+        PageHelper.startPage(chapterPageDto.getPage(),chapterPageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
+        ChapterExample.Criteria criteria = chapterExample.createCriteria();
+        if (!StringUtils.isEmpty(chapterPageDto.getCourseId())) {
+            criteria.andCourseIdEqualTo(chapterPageDto.getCourseId());
+        }
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
-        pageDto.setTotal(pageInfo.getTotal());
+        chapterPageDto.setTotal(pageInfo.getTotal());
         List<ChapterDto> chapterDtoList = CopyUtil.copyList(chapterList, ChapterDto.class);
-        pageDto.setList(chapterDtoList);
+        chapterPageDto.setList(chapterDtoList);
     }
 
     /**
